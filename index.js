968498ad46636b5c6e0c5b49ab3f1ca133396680
Ekpo David <dave170e71@gmail.com>
2020-02-20 17:55:34 +0100

    actions

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ try {
   const SLACK_AUTH_TOKEN = core.getInput('slack_auth_token');
 
   exec.exec(`yarn`)
-    .then(() => exec.exec(`yarn global add expo-cli`))
-    .then(() => exec.exec(`yarn global add firebase-tools`))
-    .then(() => exec.exec(`yarn run expo login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}`))
-    .then(() => exec.exec(`yarn build:android`))
-    .then(() => exec.exec(`tail -n 2 ./output.txt | head -n 1 | cut -c47- | xargs wget -O build.apk`))
-    .then(() => exec.exec(`firebase appdistribution:distribute ./build.apk --app ${FIREBASE_ANDROID_APP_ID} --groups internal --token ${FIREBASE_TOKEN}`))
+    .then(() => exec.exec(`yarn dev-core-web`))
     .catch(e => core.setFailed(e));
+
+    // exec.exec(`yarn`)
+    //   .catch(e => core.setFailed(e));
     
 } catch (error) {
   core.setFailed(error.message);
