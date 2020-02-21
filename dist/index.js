821f484365b399d63fd647d9937158550c5c1ebc
Ekpo David <dave170e71@gmail.com>
2020-02-21 10:10:57 +0100

    actions

--- dist/index.js
+++ dist/index.js
@@ -1624,6 +1624,7 @@ try {
   exec.exec(`git clone -b mvp https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/ezegenesis/client-core.git`)
     .then(() => exec.exec(`yarn`))
     .then(() => exec.exec(`yarn dev-core-ezepro`))
+    .then(() => exec.exec(`pkill node`))
     .catch(e => core.setFailed(e));
 
     // exec.exec(`yarn`)
