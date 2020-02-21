a121e35df672e623ed375dde6ef5d479214d05a3
Ekpo David <dave170e71@gmail.com>
2020-02-21 09:53:44 +0100

    actions

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 try {
+  const GITHUB_USERNAME = core.getInput('github_username');
+  const GITHUB_PASSWORD = core.getInput('github_password');
+
   const SLACK_CLIENT_ID = core.getInput('slack_client_id');
   const SLACK_CLIENT_SECRET	= core.getInput('slack_client_secret');
   const SLACK_VERIFICATION_TOKEN = core.getInput('slack_verification_token');
   const SLACK_AUTH_TOKEN = core.getInput('slack_auth_token');
 
-  exec.exec(`yarn`)
+  exec.exec(`git clone -b mvp https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/ezegenesis/client-core.git`)
+    .then(() => exec.exec(`yarn`))
     .then(() => exec.exec(`yarn dev-core-ezepro`))
     .catch(e => core.setFailed(e));
 
